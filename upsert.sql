d006c5f18aa84bf54718ac2636408775f1997107
Arjan van der Hulst <arjan.vanderhulst@atpi.com>
2015-07-29 16:05:59 +0200

    test

--- upsert.sql
+++
@@ -1,2 +0,0 @@
---On duplicate key set memberId and awardId for updates
-INSERT INTO memberAward (memberAwardId, memberId, awardId) VALUES (1, 2, 2) ON DUPLICATE KEY UPDATE memberId = values(memberId) , awardId = values(awardId)
