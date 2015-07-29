cc5e7c66a3e431a2b9ac01b08c3777a510b4c5c5
Arjan van der Hulst <arjan.vanderhulst@atpi.com>
2015-07-29 16:09:01 +0200

    Added php, sql and yml

---
+++ memberAward.sql
@@ -0,0 +1,37 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 192.168.1.43
+Source Server Version : 50538
+Source Host           : 192.168.1.43:3306
+Source Database       : testopdracht
+
+Target Server Type    : MYSQL
+Target Server Version : 50538
+File Encoding         : 65001
+
+Date: 2015-07-29 16:07:41
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `memberAward`
+-- ----------------------------
+DROP TABLE IF EXISTS `memberAward`;
+CREATE TABLE `memberAward` (
+  `memberAwardID` int(11) NOT NULL AUTO_INCREMENT,
+  `memberId` int(11) NOT NULL,
+  `awardId` int(11) NOT NULL,
+  PRIMARY KEY (`memberAwardID`),
+  KEY `memberId` (`memberId`),
+  KEY `awardId` (`awardId`),
+  CONSTRAINT `memberId` FOREIGN KEY (`memberId`) REFERENCES `member` (`memberId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `awardId` FOREIGN KEY (`awardId`) REFERENCES `award` (`awardId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of memberAward
+-- ----------------------------
+INSERT INTO `memberAward` VALUES ('1', '2', '1');
+INSERT INTO `memberAward` VALUES ('2', '1', '1');
